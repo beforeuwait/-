4a0e6468e3bc4a50eb60e5a0331efd898baab895
beforeuwait <forme.wjw@aliyun.com>
2017-11-05 10:08:12 +0800

    平台化
    update

--- platform/requestsModule.py
+++ platform/requestsModule.py
@@ -167,7 +167,73 @@ class getModule:
                 response = connfig.response_error
             retry -= 1
         return response
+    def __get_with_params(self, **kwargs):
+        """
+        作为有参数的 GET 请求
+        1. 参数需要 url, headers, cookies, params, allow_redirects, proxies, decode
+        2. 模仿requests源码里 将allow_redirects 的值为 True, 如果有需要将其设置为False
+        3. 处理status_code,按照不同对类型做相应对处理
+        4. 重试对次数为5
+        """
+        kwargs.setdefault('allow_redirects', True)
+        kwargs.setdefault('decode', 'utf-8')
+        response = connfig.response_error
+        retry = connfig.request_retry_times
+        while retry > 0:
+            try:
+                res = requests.get(
+                    kwargs.get('url'),
+                    headers=kwargs.get('headers'),
+                    cookies=kwargs.get('cookies'),
+                    params=kwargs.get('params'),
+                    allow_redirects=kwargs.get('allow_redirects'),
+                    proxies=kwargs.get('proxies'),
+                    timeout=connfig.timeout
+                )
+                # 这里部分对返回值做处理
+                status_code = res.status_code
+                if repr(status_code).startswith('2'):
+                    """状态码为 2xx 时候的处理方法,直接返回html"""
+                    try:
+                        response = res.content.decode(kwargs.get('decode'))
+                    except Exception as e:
+                        logging.debug('该网页不支持 %s 格式, %s' % (kwargs.get('decode'), e))
+                        response = res.text
+                    break
+                elif repr(status_code).startswith('3'):
+                    """
+                    状态码为 3xx 时候的处理方法，只有在默认 allow_redirects=True时候才会出现
+                    目前的处理思路为遇到重定向，将 allow_redirects=False 再次请求
+                    """
+                    kwargs['allow_redirects'] = False
+                    logging.info('在请求 %s 时,返回 status_code:%s,将不允许重定向' % (kwargs.get('url'), status_code))
 
+                    continue
+                elif repr(status_code).startswith('4'):
+                    """
+                    状态码为 4xx 时候处理方法，都4xx了，明确是被ban或者是网络都原因
+                    目前的处理方法就是 
+                            1.有代理的情况下，更改代理ip地址，再请求
+                            2.没有代理，那直接跳过
+                    """
+                    if kwargs.get('proxies'):
+                        kwargs.get('headers')['Proxy-Switch-Ip'] = 'yes'
+                        continue
+                    else:
+                        logging.info('在请求 %s 时,返回 status_code:%s' % (kwargs.get('url'), status_code))
+                        break
+                elif repr(status_code).startswith('5'):
+                    """
+                    状态码为 5xx 时候处理方法，5xx 服务器的内部错误
+                    暂时的处理方法,同 4xx 无代理一致
+                    """
+                    break
+            except Exception as e:
+                logging.info('请求过程中出错,%s' % e)
+                response = connfig.response_error
+            retry -= 1
+        return response
+    
     def response_parse(self, response):
         """
         返回response对象，然后通过 状态码的判断，从而相应的处理
