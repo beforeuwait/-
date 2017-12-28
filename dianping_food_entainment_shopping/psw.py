55017c6fd27d2285325704f5c749ca0404d1c91d
beforeuwait <forme.wjw@aliyun.com>
2017-12-28 09:04:08 +0800

    大众点评

---
+++ dianping_food_entainment_shopping/psw.py
@@ -0,0 +1,16 @@
+import re
+
+
+psw = 'dnaondiua11w123'
+
+result = re.findall('.{2,5}', psw)
+print(result)
+
+class demo:
+    def __init__(self) -> None:
+        pass
+
+    def demo(self, a: 'int > 0'=80)->str:
+        print(a)
+d = demo()
+d.demo()
